344580dbdf37fd2d40e470f17f7ed3747eefc97f
Andreas Bolstad <26123972+andreasbolstad@users.noreply.github.com>
2024-12-24 22:51:01 +0100

    feat(day01): solve day 1 part 2

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,4 +1,4 @@
-def main():
+def part1():
     with open("input") as f:
         lines = f.readlines()
         col1, col2 = list(zip(*(line.split() for line in lines)))
@@ -6,5 +6,16 @@ def main():
         print(distance)
 
 
+def part2():
+    from collections import Counter
+    with open("input") as f:
+        lines = f.readlines()
+        col1, col2 = zip(*(map(int, line.split()) for line in lines))
+        s1 = set(col1)
+        s2 = Counter(col2)
+        similarity_score = sum([s2[k]*k for k in s1])
+        print(similarity_score)
+
+
 if __name__ == "__main__":
-    main()
+    part2()
